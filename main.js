fc95111f425c838ae1ed07ceb4600aa00ea3ec81
YOUSSEF WALID <eljoewalid@gmail.com>
2026-09-13 20:45:10 +0300

    validation: name, email , age

--- main.js
+++ main.js
@@ -20,3 +20,35 @@ function theme() {
     toggleBtn.classList.toggle("bi-moon-fill");
 
 }
+
+
+function validateForm() {
+
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let age = document.getElementById("age").value;
+
+    if (name == "") {
+        alert("Please enter your name");
+        return false;
+    }
+
+    if (email == "") {
+        alert("Please enter your email");
+        return false;
+    }
+
+    if (phone.length != 11) {
+        alert("Phone number must be 11 digits");
+        return false;
+    }
+
+    if (age < 18) {
+        alert("Age must be 18 or above");
+        return false;
+    }
+
+    alert("Validation successful!");
+    return true;
+}
